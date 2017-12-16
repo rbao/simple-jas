4cdee263145296752ad5a29942087ed913673daa
Roy Bao <roy@roybao.com>
2017-12-15 22:16:12 -0800

    Add build related files

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0
+  }
+}
